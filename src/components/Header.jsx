8c8a2c57a342e83a9a774fe60a2f188ba7cb4124
Spasidar Hadzhipase <shadjipasev@gmail.com>
2023-11-16 21:42:33 +0200

    Cmt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 import logo from "../assets/logo.png";
-
+import "../styles/header.css";
 export default function Header() {
   return (
     <header>
-      <div className="header__left">
-        <img src={logo} alt="" className="left__logo" />
-        <h3 className="header__left__moto">
+      <div className="head__left">
+        <img src={logo} alt="" className="head__left__logo" />
+        <h3 className="head__left__moto">
           Пенливи изненади за вашите вкусове и портфейл!
         </h3>
       </div>
 
-      <div className="header__right">
+      <div className="head__right">
         {/* <Routes>
             
         </Routes> */}
-        <a href="" class className="header__right__nav">
+        <a href="" className="head__right__nav">
           Каталози
         </a>
-        <a href="" class className="header__right__nav">
+        <a href="" className="head__right__nav">
           Магазини
         </a>
-        <a href="" class className="header__right__nav">
+        <a href="" className="head__right__nav">
           Оферти
         </a>
         {/* Here the user will let the luck choose what beer he will drink */}
-        <a href="" class className="header__right__nav" />
-        Тото Бира
+        <a href="" className="head__right__nav"></a>
+        {/* Тото Бира */}
       </div>
     </header>
   );
