d2755197a17698a511f032b19da7201ee9088b47
Spasidar Hadzhipase <shadjipasev@gmail.com>
2023-11-18 14:45:49 +0200

    Initial version of Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,21 @@ import "../styles/sidebar.css";
 export default function () {
   return (
     <div className="sidebar">
-      <h1>We are in SideBar</h1>
+      <div className="sidebar__nav">
+        <a href="" className="sidebar__nav__link">
+          Каталози
+        </a>
+        <a href="" className="sidebar__nav__link">
+          Магазини
+        </a>
+        <a href="" className="sidebar__nav__link">
+          Оферти
+        </a>
+        {/* Here the user will let the luck choose what beer he will drink */}
+        <a href="" className="sidebar__nav__link">
+          Тото Бира
+        </a>
+      </div>
     </div>
   );
 }
