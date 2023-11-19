5bdbd05354840087ede83b254704183c8e13db9d
Spasidar Hadzhipase <shadjipasev@gmail.com>
2023-11-19 11:11:55 +0200

    Initial structure of main cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,68 @@
 import "../styles/main.css";
+import beer from "../assets/Бургаско.png";
 
 export default function Main() {
   return (
     <div className="main">
-      <h1>We are in main!</h1>
+      <h1 className="main__title">You are in main</h1>
+
+      <section className="main__section">
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+        <div className="main__card">
+          <img className="main__card__img" src={beer} alt="" />
+          <p className="main__card__product">Бургаско-Кен</p>
+          <p className="main__card__price">1.60 лв.</p>
+        </div>
+      </section>
     </div>
   );
 }
