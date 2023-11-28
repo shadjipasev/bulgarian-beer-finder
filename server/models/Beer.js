f6a193548a1a7c0b75ffe0d3a6b1707d5fb2be41
Spasidar Hadzhipase <shadjipasev@gmail.com>
2023-11-28 21:33:10 +0200

    beer schema

---
+++ server/models/Beer.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const beerSchema = new Schema({
+  name: { type: String, required: true },
+  quantity: { type: String, required: true },
+  price: { type: Number, required: true },
+  oldPrice: { type: Number, required: true },
+  picUrl: { type: String, required: true },
+  validFrom: { type: Date, required: true },
+  validUntil: { type: Date, require: true },
+});
+
+const Beer = model("Beer", beerSchema);
+
+module.exports = Beer;
