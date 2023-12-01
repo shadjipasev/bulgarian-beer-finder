a7c7b70d306741fadb0511eece8d476b0a0a59ce
Spasidar Hadzhipase <shadjipasev@gmail.com>
2023-12-01 19:48:23 +0200

    cmt

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,24 +1,32 @@
+import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 import "./login.css";
 
 export default function Login() {
+  const { values, onChange, onSubmit } = useForm({
+    name: "",
+    password: "",
+  });
+
   return (
     <div className="login-page">
       <div className="form">
-        <form className="register-form">
-          <input type="text" placeholder="name" />
-          <input type="password" placeholder="password" />
-          <input type="text" placeholder="email address" />
-          <button>create</button>
-          <p className="message">
-            Already registered? <a href="#">Sign In</a>
-          </p>
-        </form>
-        <form className="login-form">
-          <input type="text" placeholder="username" />
-          <input type="password" placeholder="password" />
+        <form className="login-form" onSubmit={onSubmit}>
+          <input
+            type="text"
+            placeholder="username"
+            value={values.name}
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={values.password}
+            onChange={onChange}
+          />
           <button>login</button>
           <p className="message">
-            Not registered? <a href="#">Create an account</a>
+            Not registered? <Link to="/register">Create an account</Link>
           </p>
         </form>
       </div>
